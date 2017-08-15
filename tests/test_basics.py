59e88d97c18514ae7374d98be09acacfc10fe16d
Adriaan <adriaan.rol@gmail.com>
2017-08-15 10:26:22 +0200

    Converted tabs to spaces in test files

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,54 +1,55 @@
 from openql import openql as ql
 import unittest
 
+
 class Test_basic(unittest.TestCase):
 
-	def test_compilation(self):
-		# set global options kernel
-		ql.set_instruction_map_file("instructions.map")
-		ql.init()
+    def test_compilation(self):
+        # set global options kernel
+        ql.set_instruction_map_file("instructions.map")
+        ql.init()
 
-		# populate kernel
-		k = ql.Kernel("aKernel")
-		k.prepz(0)
-		k.prepz(1)
-		k.identity(0)
-		k.identity(1)
-		k.measure(0)
-		k.measure(1)
+        # populate kernel
+        k = ql.Kernel("aKernel")
+        k.prepz(0)
+        k.prepz(1)
+        k.identity(0)
+        k.identity(1)
+        k.measure(0)
+        k.measure(1)
 
-		sweep_points = [2]
-		num_circuits = 1
-		nqubits = 2
+        sweep_points = [2]
+        num_circuits = 1
+        nqubits = 2
 
-		p = ql.Program("aProgram", nqubits)
-		p.set_sweep_points(sweep_points, num_circuits)
-		p.add_kernel(k)
-		p.compile(False, True)
+        p = ql.Program("aProgram", nqubits)
+        p.set_sweep_points(sweep_points, num_circuits)
+        p.add_kernel(k)
+        p.compile(False, True)
 
-	def test_scheduling(self):
-		# set global options kernel
-		ql.set_instruction_map_file("instructions.map")
-		ql.init()
+    def test_scheduling(self):
+        # set global options kernel
+        ql.set_instruction_map_file("instructions.map")
+        ql.init()
 
-		# populate kernel
-		k = ql.Kernel("aKernel")
-		k.prepz(0)
-		k.prepz(1)
-		k.identity(0)
-		k.identity(1)
-		k.measure(0)
-		k.measure(1)
+        # populate kernel
+        k = ql.Kernel("aKernel")
+        k.prepz(0)
+        k.prepz(1)
+        k.identity(0)
+        k.identity(1)
+        k.measure(0)
+        k.measure(1)
 
-		sweep_points = [2]
-		num_circuits = 1
-		nqubits = 2
+        sweep_points = [2]
+        num_circuits = 1
+        nqubits = 2
 
-		p = ql.Program("aProgram", nqubits)
-		p.set_sweep_points(sweep_points, num_circuits)
-		p.add_kernel(k)
-		p.compile(False, True)
-		p.schedule()
+        p = ql.Program("aProgram", nqubits)
+        p.set_sweep_points(sweep_points, num_circuits)
+        p.add_kernel(k)
+        p.compile(False, True)
+        p.schedule()
 
 
 if __name__ == '__main__':
--- tests/test_qubits.py
+++ tests/test_qubits.py
@@ -5,86 +5,87 @@ from openql import openql as ql
 
 rootDir = os.path.dirname(os.path.realpath(__file__))
 
+
 class Test_qubits(unittest.TestCase):
 
-	def test_1_qubit(self):
-		ql.set_instruction_map_file("instructions.map")
-		ql.init()
+    def test_1_qubit(self):
+        ql.set_instruction_map_file("instructions.map")
+        ql.init()
 
-		k = ql.Kernel("aKernel")
+        k = ql.Kernel("aKernel")
 
-		# populate kernel
-		k.prepz(0);
-		k.x(0);
-		k.y(0);
-		k.rx90(0);
-		k.measure(0);
+        # populate kernel
+        k.prepz(0)
+        k.x(0)
+        k.y(0)
+        k.rx90(0)
+        k.measure(0)
 
-		nqubits = 1
-		sweep_points = [2]
-		num_circuits = 1
-		p = ql.Program("aProgram", nqubits)
-		p.set_sweep_points(sweep_points, num_circuits)
+        nqubits = 1
+        sweep_points = [2]
+        num_circuits = 1
+        p = ql.Program("aProgram", nqubits)
+        p.set_sweep_points(sweep_points, num_circuits)
 
-		p.add_kernel(k) # add kernel to program
-		p.compile()     # compile program
+        p.add_kernel(k)  # add kernel to program
+        p.compile()     # compile program
 
-		gold = rootDir + '/golden/test_1_qubit.qasm'
-		isSame = filecmp.cmp('output/aProgram.qasm', gold)
-		self.assertTrue(isSame)
+        gold = rootDir + '/golden/test_1_qubit.qasm'
+        isSame = filecmp.cmp('output/aProgram.qasm', gold)
+        self.assertTrue(isSame)
 
-	def test_2_qubit(self):
-		ql.set_instruction_map_file("instructions.map")
-		ql.init()
+    def test_2_qubit(self):
+        ql.set_instruction_map_file("instructions.map")
+        ql.init()
 
-		k = ql.Kernel("aKernel")
+        k = ql.Kernel("aKernel")
 
-		# populate kernel
-		k.prepz(0);
-		k.prepz(1);
-		k.prepz(2);
-		k.cnot(0,1);
-		k.clifford(1,2);
-		k.measure(2);
+        # populate kernel
+        k.prepz(0)
+        k.prepz(1)
+        k.prepz(2)
+        k.cnot(0, 1)
+        k.clifford(1, 2)
+        k.measure(2)
 
-		nqubits = 3
-		sweep_points = [2]
-		num_circuits = 1
-		p = ql.Program("aProgram", nqubits)
-		p.set_sweep_points(sweep_points, num_circuits)
+        nqubits = 3
+        sweep_points = [2]
+        num_circuits = 1
+        p = ql.Program("aProgram", nqubits)
+        p.set_sweep_points(sweep_points, num_circuits)
 
-		p.add_kernel(k) # add kernel to program
-		p.compile()     # compile program
+        p.add_kernel(k)  # add kernel to program
+        p.compile()     # compile program
 
-		gold = rootDir + '/golden/test_2_qubit.qasm'
-		isSame = filecmp.cmp('output/aProgram.qasm', gold)
-		self.assertTrue(isSame)
+        gold = rootDir + '/golden/test_2_qubit.qasm'
+        isSame = filecmp.cmp('output/aProgram.qasm', gold)
+        self.assertTrue(isSame)
 
-	def test_3_qubit(self):
-		ql.set_instruction_map_file("instructions.map")
-		ql.init()
+    def test_3_qubit(self):
+        ql.set_instruction_map_file("instructions.map")
+        ql.init()
 
-		k = ql.Kernel("aKernel")
+        k = ql.Kernel("aKernel")
 
-		# populate kernel
-		k.prepz(0);
-		k.prepz(1);
-		k.prepz(2);
-		k.toffoli(0,1,2);
-		k.measure(2);
+        # populate kernel
+        k.prepz(0)
+        k.prepz(1)
+        k.prepz(2)
+        k.toffoli(0, 1, 2)
+        k.measure(2)
 
-		nqubits = 3
-		sweep_points = [2]
-		num_circuits = 1
-		p = ql.Program("aProgram", nqubits)
-		p.set_sweep_points(sweep_points, num_circuits)
+        nqubits = 3
+        sweep_points = [2]
+        num_circuits = 1
+        p = ql.Program("aProgram", nqubits)
+        p.set_sweep_points(sweep_points, num_circuits)
 
-		p.add_kernel(k) # add kernel to program
-		p.compile()     # compile program
+        p.add_kernel(k)  # add kernel to program
+        p.compile()     # compile program
 
-		gold = rootDir + '/golden/test_3_qubit.qasm'
-		isSame = filecmp.cmp('output/aProgram.qasm', gold)
-		self.assertTrue(isSame)
+        gold = rootDir + '/golden/test_3_qubit.qasm'
+        isSame = filecmp.cmp('output/aProgram.qasm', gold)
+        self.assertTrue(isSame)
 
 if __name__ == '__main__':
     unittest.main()
