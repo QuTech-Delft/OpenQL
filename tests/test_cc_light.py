1a5dd470a6c8826ab0a06936bbd0f58befb72182
Imran Ashraf <iimran.aashraf@gmail.com>
2017-09-04 10:20:21 +0200

    added various single/two qubit cc-light tests

--- tests/test_cc_light.py
+++ tests/test_cc_light.py
@@ -7,46 +7,154 @@ curdir    = os.path.dirname(__file__)
 output_dir = os.path.join(curdir, 'test_output')
 ql.set_output_dir(output_dir)
 
-def first_example():
-    # You can specify a config location, here we use a default config
-    config_fn = os.path.join(curdir, 'hardware_config_cc_light.json')
-    platform  = ql.Platform('seven_qubits_chip', config_fn)
-    sweep_points = [1,2]
-    num_circuits = 2
-    num_qubits = 7
-    p = ql.Program('my_program', num_qubits, platform)
-    p.set_sweep_points(sweep_points, num_circuits)
+class Test_basic(unittest.TestCase):
 
-    # populate kernel using default gates
-    k = ql.Kernel('first_kernel', platform)
-    k.prepz(0)
-    k.x(2)
-    k.y(3)
-    k.z(5)
-    k.rx90(0)
-    # k.cphase(0,1)  # when uncommented this should raise 'operation not supported' exception. 
-    k.cphase(0,3)
-    k.measure(0)
+    # single qubit mask generation test
+    # @unittest.expectedFailure
+    # @unittest.skip
+    def test_smis(self):
+        # You can specify a config location, here we use a default config
+        config_fn = os.path.join(curdir, 'hardware_config_cc_light.json')
+        platform  = ql.Platform('seven_qubits_chip', config_fn)
+        sweep_points = [1,2]
+        num_circuits = 1
+        num_qubits = 7
+        p = ql.Program('my_program', num_qubits, platform)
+        p.set_sweep_points(sweep_points, num_circuits)
 
-    # add the kernel to the program
-    p.add_kernel(k)
+        # populate kernel using default gates
+        k = ql.Kernel('first_kernel', platform)
+        k.prepz(0)
+        k.prepz(1)
+        k.prepz(2)
+        k.prepz(3)
+        k.hadamard(0)
+        k.hadamard(1)
+        k.x(2)
+        k.x(3)
+        k.measure(0)
+        k.measure(1)
+        k.measure(2)
+        k.measure(3)
 
-    # populate a second kernel using both custom and default gates
-    k = ql.Kernel('second_kernel', platform)
-    k.gate('prepz', 0) # this line is equivalent to the previous
-    k.gate('x', 0)
-    k.y(1)
-    k.z(5)
-    k.gate('rx90', 0)
-    k.gate('cz', [0, 3])
-    k.gate('measz', 0)
+        # add the kernel to the program
+        p.add_kernel(k)
 
-    # add the kernel to the program
-    p.add_kernel(k)
+        # compile the program
+        p.compile(optimize=False, verbose=True)
 
-    # compile the program
-    p.compile(optimize=False, verbose=False)
+
+    # single qubit mask generation test with custom gates
+    # @unittest.skip
+    def test_smis_with_custom_gates(self):
+        # You can specify a config location, here we use a default config
+        config_fn = os.path.join(curdir, 'hardware_config_cc_light.json')
+        platform  = ql.Platform('seven_qubits_chip', config_fn)
+        sweep_points = [1,2]
+        num_circuits = 2
+        num_qubits = 7
+        p = ql.Program('my_program', num_qubits, platform)
+        p.set_sweep_points(sweep_points, num_circuits)
+
+        # populate the second kernel using both custom and default gates
+        k = ql.Kernel('first_kernel', platform)
+        k.gate('prepz', 0) # this line is equivalent to the previous
+        k.gate('x', 0)
+        k.y(1)
+        k.z(5)
+        k.gate('rx90', 0)
+        k.gate('cnot', [0, 3])
+        k.gate('measz', 0)
+
+        # add the kernel to the program
+        p.add_kernel(k)
+
+        # compile the program
+        p.compile(optimize=False, verbose=False)
+
+
+    # single qubit mask generation multi-kernel test (custom with non-custom gates)
+    @unittest.skip
+    def test_smis_multi_kernel(self):
+        # You can specify a config location, here we use a default config
+        config_fn = os.path.join(curdir, 'hardware_config_cc_light.json')
+        platform  = ql.Platform('seven_qubits_chip', config_fn)
+        sweep_points = [1,2]
+        num_circuits = 2
+        num_qubits = 7
+        p = ql.Program('my_program', num_qubits, platform)
+        p.set_sweep_points(sweep_points, num_circuits)
+
+        # populate the first kernel using default gates
+        k = ql.Kernel('first_kernel', platform)
+        k.prepz(0)
+        k.x(2)
+        k.y(3)
+        k.z(5)
+        k.rx90(0)
+        # k.cphase(0,1)  # when uncommented this should raise 'operation not supported' exception. 
+        # k.cphase(0,3)
+        k.cnot(0,3)
+        k.measure(0)
+
+        # add the kernel to the program
+        p.add_kernel(k)
+
+        # populate the second kernel using both custom and default gates
+        k = ql.Kernel('first_kernel', platform)
+        k.gate('prepz', 0) # this line is equivalent to the previous
+        k.gate('x', 0)
+        k.y(1)
+        k.z(5)
+        k.gate('rx90', 0)
+        # k.gate('cz', [0, 3])
+        k.gate('cnot', [0, 3])
+        k.gate('measz', 0)
+
+        # add the kernel to the program
+        p.add_kernel(k)
+
+        # compile the program
+        p.compile(optimize=False, verbose=False)
+
+
+    # two qubit mask generation test
+    # @unittest.skip
+    def test_smit(self):
+        # You can specify a config location, here we use a default config
+        config_fn = os.path.join(curdir, 'hardware_config_cc_light.json')
+        platform  = ql.Platform('seven_qubits_chip', config_fn)
+        sweep_points = [1,2]
+        num_circuits = 1
+        num_qubits = 7
+        p = ql.Program('my_program', num_qubits, platform)
+        p.set_sweep_points(sweep_points, num_circuits)
+
+        # populate kernel using default gates
+        k = ql.Kernel('first_kernel', platform)
+        k.prepz(0)
+        k.prepz(1)
+        k.prepz(2)
+        k.prepz(3)
+        k.hadamard(0)
+        k.hadamard(1)
+        k.x(2)
+        k.x(3)
+        k.cnot(2,0)
+        k.cnot(2,0)
+        k.cnot(1,4)
+        k.measure(0)
+        k.measure(1)
+        k.measure(2)
+        k.measure(3)
+
+        # add the kernel to the program
+        p.add_kernel(k)
+
+        # compile the program
+        p.compile(optimize=False, verbose=False)
+
 
 
 if __name__ == '__main__':
-    first_example()
+    unittest.main()
