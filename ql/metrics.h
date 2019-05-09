eb620f21ec5923b8a8e1b0abd8317ca061c8284b
Diogo Valada <diogovalada.7@tecnico.ulisboa.pt>
2019-05-09 13:11:25 +0200

    Some more work on Depolarizing Model. Close to done, needs error transformation on gates (a.k.a completing single_qubit_gate and two_qubit_gate functions)

--- ql/metrics.h
+++ ql/metrics.h
@@ -12,7 +12,6 @@
 #include <cmath> 
 #include <vector>
 #include <map>
-#include <unordered_map>
 #include <random>
 #include <chrono>
 #include "gate.h"
@@ -192,7 +191,7 @@ public:
 			else if (gate->name == "prep_z")
 			{
 				size_t qubit = gate->operands[0]; 
-				fids[qubit] = 1.0;
+				split_sets(qubit);
 				last_op_endtime[qubit] = gate->cycle + gate->duration / CYCLE_TIME;
 				continue;
 			}
@@ -292,16 +291,96 @@ public:
 // {
 // 	//Each physical qubit should be associated to only one QubitSet
 // 	private:
-// 		std::unordered_map<std::string, double> error_list; //the string contains the error status of each Qubit. Ex: IIXYXXI for a qubit set with 7 qubits 
-// 		//use map instead if all error strings are generated and map pattern is known? (map is also unordered apparently, use vector of std::pairs instead)
-// 		std::vector<size_t> qubits; //Contains the qubits ordered as they would appear in a error string
-
+// 		std::map<std::string, double> error_map; //the string contains the error status of each Qubit. Ex: IIXYXXI for a qubit set with 7 qubits 
 // 	public:
-// 	QubitSet( std::vector<size_t> qubits )
-// 	{
-// 		error_list.reserve( qubits.size() );
-// 		this->qubits = qubits;
-// 	}
+// 		std::vector<size_t> qubits; //Contains the qubits ordered as they would appear in a error string
+// 		static double new_error_threshold;
+
+// 		QubitSet( std::vector<size_t> qubits )
+// 		{
+// 			// error_map.reserve( qubits.size() );
+// 			this->qubits = qubits;
+// 			reset();
+// 		}
+
+// 		QubitSet( size_t qubit )
+// 		{
+// 			// error_map.reserve( qubits.size() );
+// 			this->qubits.push_back(qubit);
+// 			reset();
+// 		}
+
+// 		//Constructor for merging sets, doesn't use reset
+// 		QubitSet( QubitSet* qubit_set1, QubitSet* qubit_set2 )
+// 		{
+
+// 			// for (auto& error : qubit_set2->error_map) //Wrong, we need to do the cross product
+//   			// 	error_map.insert(std::move(error));
+// 			for (auto & error1 : qubit_set1->error_map)
+// 			{
+// 				for (auto & error2 : qubit_set2->error_map )
+// 				{
+// 					double joint_probability = error1.second * error2.second;
+// 					if ( joint_probability > 0.01)
+// 						error_map[error1.first + error2.first] = joint_probability; 
+// 				}
+// 			}
+// 			// error_map.push_back(std::move(qubit_set2->error_map));
+			
+// 			this->qubits = qubit_set1->qubits;
+// 			std::move(qubit_set2->qubits.begin(), qubit_set2->qubits.end(), std::back_inserter(this->qubits));
+		
+
+// 		}
+
+// 		void reset()
+// 		{
+// 			std::string initial_state(qubits.size(), 0);
+// 			error_map[initial_state] = 1.0;
+// 		}
+
+
+// 		void remove_qubit(size_t qubit)
+// 		{
+// 			size_t q_index = get_qubit_index(qubit)
+			
+// 			for (auto & error : error_map)
+// 			{
+// 				//Since map is ordered by string, this can probably be optimized, by checking the next 3 keys and verifying if they are variations
+// 				//^ This would probably be faster than map::find()
+// 				std::string error_string = error.first; 
+// 				unsigned char original = error.first.at(q_index);
+// 				unsigned char variations[3];
+
+// 				if (original == 0)
+// 					variations = {1,2,3};
+// 				else if (original == 1)
+// 					variations = {0,2,3};
+// 				else if (original == 2)
+// 					variations = {0,1,3};
+// 				else if (original == 3)
+// 					variations = {0,1,2};
+
+// 				for (auto variant : variations)
+// 				{
+// 					error_string.at(q_index) = variant;
+// 					auto it = error_map.find(error_string);
+					
+// 					if ( it != error_map.end() )
+// 					{
+// 						//If variation is found, its probability is summed to original string, and it is deleted
+// 						error.second += it->second; 
+// 						error_map.erase(it);
+// 					}
+
+// 				}
+// 				//The probabilities are now funneled onto this error.
+// 				//Now we erase the qubit from this string.
+// 				error.first.erase(q_index, 1);
+// 			}
+// 			//Finally, we remove the qubit from the qubit_list
+// 			qubits.erase(qubits.begin() + q_index);
+// 		}
 
 
 // 	// void introduce_wait_cycles(size_t cycles, size_t qubit)
@@ -312,53 +391,67 @@ public:
 // 	// 	}
 // 	// }
 
-// 	size_t qubit_index(size_t qubit)
-// 	{
-
-// 		std::vector<size_t>::iterator it = std::find(qubits.begin(), qubits.end(), 22);
-
-// 		if (it == qubits.end())
+// 		size_t get_qubit_index(size_t qubit)
 // 		{
-// 			DOUT << "Qubit not found in expected QubitSet" << std::endl;
-// 			throw ql::exception("Qubit not found in expected QubitSet", false);
+
+// 			std::vector<size_t>::iterator it = std::find(qubits.begin(), qubits.end(), 22);
+
+// 			if (it == qubits.end())
+// 			{
+// 				DOUT("Qubit not found in expected QubitSet \n");
+// 				throw ql::exception("Qubit not found in expected QubitSet", false);
+// 			}
+
+// 			return std::distance(qubits.begin(), it);
 // 		}
 
-// 		return std::distance(qubits.begin(), it);
-// 	}
-
-// 	void introduce_single_qubit_error(size_t qubit, double fid)
-// 	{
-// 		double axis_flip_prob = (1-fid)/3;
-		
-// 		for (auto & error : error_list)
+// 		void single_qubit_gate(size_t qubit, double fid, const std::string &name)
 // 		{
-// 			char current_state = error.first.at( qubit_index(qubit) ); //Gets error status for qubit at a error string. Replace .at() for [] for optimization
+// 			//TODO: Transform previous errors first
+// 			introduce_single_qubit_error(qubit, fid); 
+// 		}
 
+// 		void two_qubit_gate(size_t qubit, double fid, const std::string &name, int control_target)
+// 		{
+// 			//TODO: Transform previous errors first
+// 			introduce_single_qubit_error(qubit, fid); 
+// 		}
 
-// 			for (auto error_type = 1; error_type < 4; error_type++)
+// 		void introduce_single_qubit_error(size_t qubit, double fid)
+// 		{
+// 			double axis_flip_prob = (1-fid)/3;
+// 			std::map< std::string, double> new_error_map;
+			
+// 			for (auto & error : error_map)
 // 			{
-// 				unsigned char new_state = new_state_calc(current_state, error_type);
-// 				double new_state_prob = error.second * axis_flip_prob;
 
-
-// 				std::string new_error_syndrome(error.first);
-// 				new_error_syndrome.at( qubit_index(qubit) ) = new_state; //string with the new error syndrome, for each case
+// 				char current_state = error.first.at( get_qubit_index(qubit) ); //Gets error status for qubit at a error string. Replace .at() for [] for optimization
 				
-// 				auto ret = error_list.insert(std::pair<std::string, double>(new_error_syndrome, new_prob)); //only adds if error syndrome not on the list
-// 				if (ret.second == false) //If a key already existed, then
-// 					error_list[new_error_syndrome] += new_state_prob; //This is wrong, I still need the un-updated probability.
 
-		
+// 				// new_error_map.insert(std::pair<std::string, double>(error, error.second * fid)); //Inserts the new states for a 'I' injected error
+// 				// use pointers and the new char instead of the string?
+				
+// 				new_error_map.insert(std::pair<std::string, double>(error.first, error.second * fid));
 
+// 				for (auto new_error = 1; new_error < 4; new_error++)
+// 				{
+// 					unsigned char new_state = new_state_calc(current_state, new_error);
+// 					double new_state_prob = error.second * axis_flip_prob;
 
+// 					std::string new_error_string(error.first);
+// 					new_error_string.at( get_qubit_index(qubit) ) = new_state; //string with the new error syndrome, for each case
+					
+// 					if (new_state_prob > new_error_threshold )
+// 					{
+// 						auto ret = new_error_map.insert(std::pair<std::string, double>(new_error_string, new_state_prob)); //only adds if error syndrome not on the list
+// 						if (ret.second == false) //If a key already existed, then
+// 							error_map[new_error_string] += new_state_prob; 
+// 					}
+// 				}
 
 // 			}
-			
-// 			//Update error value for the case of an Identity error gate
-// 			error.second *= fid;
-
+// 			error_map = new_error_map;
 // 		}
-// 	}
 
 
 
@@ -373,47 +466,206 @@ public:
 // 		size_t Nqubits;
 
 // 	public:
-// 		Depolarizing_model(size_t Nqubits, double gatefid1, double gatefid2): gatefid1(gatefid1), gatefid2(gatefid2)
+// 		Depolarizing_model(size_t Nqubits, double gatefid1, double gatefid2, double new_error_threshold): gatefid1(gatefid1), gatefid2(gatefid2)
 // 		{
 // 			qubit_partition.reserve(Nqubits);
+// 			QubitSet::new_error_threshold = new_error_threshold; 
+
 // 			for (size_t i = 0; i<Nqubits; i++)
-// 				qubit_partition.push_back(new QubitSet);
+// 			{
+// 				std::vector<size_t> initial_qubit (1, (size_t)1);
+// 				qubit_partition.push_back(new QubitSet(  initial_qubit ));
+// 			}
 // 			this->Nqubits = Nqubits;
 // 		}
 
-// 		// char error_sampler(double fid)
-// 		// {
-// 		// 	char error;
-// 		// 	double sample = dice();
-// 		// 	if (sample > fid)
-// 		// 		error = 'I';
-// 		// 	else if (error > 2*fid/3)
-// 		// 		error = 'X';
-// 		// 	else if (error > fid/3)
-// 		// 		error = 'Y';
-// 		// 	else
-// 		// 		error = 'Z';
-// 		// 	return error;
-// 		// }
-
-// 		char introduce_error(double fid)
+// 		void merge_sets(size_t qubit1, size_t qubit2)
 // 		{
-// 			char error;
-// 			double sample = dice();
-// 			if (sample > fid)
-// 				error = 'I';
-// 			else if (error > 2*fid/3)
-// 				error = 'X';
-// 			else if (error > fid/3)
-// 				error = 'Y';
-// 			else
-// 				error = 'Z';
-// 			return error;
+// 			QubitSet* qubit_set1 = qubit_partition.at(qubit1);
+// 			QubitSet* qubit_set2 = qubit_partition.at(qubit2);
+
+// 			QubitSet* new_set = new QubitSet(qubit_set1, qubit_set2);
+
+// 			delete qubit_set1; //We destroy the old qubit sets
+// 			delete qubit_set2;
+
+// 			for (auto & qubit : new_set->qubits)
+// 			{
+// 				qubit_partition[qubit] = new_set; //Change the listing in the qubits partition
+// 			}
+// 		}
+
+// 		void split_sets(size_t qubit1)
+// 		{
+// 			QubitSet* qubit_set1 = qubit_partition.at(qubit1);
+
+			
+// 			if (qubit_set1->qubits.size() == 1 && qubit_set1->qubits.at(0) != qubit_1)
+// 				DOUT("Something seriously wrong happened: splitting qubit from the wrong, trivial set")
+// 			else if (qubit_set1->qubits.size() == 1)
+// 			{
+// 				DOUT("This shouldn't happen: Spliting qubit from trivial set")
+// 				return; //Just a safety, no sense moving a qubit to a new set when it is alone already...
+// 			}
+
+// 			QubitSet* new_set = new QubitSet(qubit1);
+
+// 			qubit_partition[qubit_1] = new_set; //Change the listing in the qubits partition
+// 			qubit_set1->remove_qubit(qubit1);
+
+// 			return;
 // 		}
 
 // 		double run_circuit(ql::circuit &circ)
+// { 
+// 		//this function considers the primitive gates! each operand undergoing a 2-qubit operation is always considered to have the same latency
+// 		//same end fidelity considered for the two operands of the same 2-qubit gate
+// 		//TODO - URGENT!! Check if gate->cycle starts in zero;
+// 		//TODO - URGENT!! do not consider the fidelity of non-used qubits (set to 2/-1?)
+
+// 		// if (fids.size() == 0)
+// 		// {
+// 		// 	IOUT("EMPTY VECTOR - Initializing. Nqubits = " + std::to_string(Nqubits));
+// 		// 	fids.resize(Nqubits, 1.0); //Initiallize a fidelity vector, if one is not provided
+// 		// 	//TODO: non initialized qubits should have undefined fidelity. It shouldn't be taken into account.
+// 		// }
+
+// 		std::vector<size_t> last_op_endtime(Nqubits, 1); //First cycle has index 1
+
+// 		PRINTER(last_op_endtime);
+// 		IOUT("\n\n");
+
+// 		for (auto &gate : circ)
 // 		{
+
+// 			if (gate->name == "measure")
+// 				continue;
+// 			else if (gate->name == "prep_z")
+// 			{
+// 				size_t qubit = gate->operands[0]; 
+// 				// fids[qubit] = 1.0;
+// 				split_qubits(qubit);
+				
+// 				last_op_endtime[qubit] = gate->cycle + gate->duration / CYCLE_TIME;
+// 				continue;
+// 			}
+			
+// 			if (gate->duration > CYCLE_TIME*2 && gate->name!="prep_z" && gate->name!="measure" )
+// 			{
+// 				EOUT("Gate with duration larger than CYCLE_TIME*20 detected! Non primitive?: " << output_mode);
+//     			throw ql::exception("Check for non primitive gates at cycle "  + std::to_string(gate->cycle) + "!", false);
+// 			}
+
+			
+// 			unsigned char type_op = gate->operands.size(); // type of operation (1-qubit/2-qubit)
+// 			if (type_op == 1)
+// 			{
+// 				size_t qubit = gate->operands[0];
+// 				size_t last_time = last_op_endtime[qubit];
+// 				IOUT("Gate " + gate->name + "("+ std::to_string(gate->operands[0]) +") at cycle " + std::to_string(gate->cycle) + " with duration " + std::to_string(gate->duration));
+// 				size_t idled_time = gate->cycle - last_time; //get idlying time to introduce decoherence. This assumes "cycle" starts at zero, otherwise gate->cycle-> (gate->cycle - 1)
+				
+// 				last_op_endtime[qubit] = gate->cycle  + gate->duration / CYCLE_TIME; //This assumes "cycle" starts at zero, otherwise gate->cycle-> (gate->cycle - 1)
+				
+// 				IOUT("Idled time:" + std::to_string(idled_time));
+
+
+// 				// fids[qubit] *= std::exp(-((double)idled_time)/decoherence_time); // Update fidelity with idling-caused decoherence
+// 				// fids[qubit] *= gatefid_1; //Update fidelity after gate
+				
+// 				//TODO: OPTIMIZE THE DECOHERENCE PART?
+// 				for (auto i = 0; i < idled_time; i++)
+// 				{
+// 					qubit_partition[qubit]->introduce_single_qubit_error(qubit, std::exp(-(double)1.0/decoherence_time)); //Introduces wait gates
+// 				} 
+// 				qubit_partition[qubit]->single_qubit_gate(qubit, gatefid_1, gate->name); //Introduces gate fidelity
+				
+				
+				
+				
+// 				IOUT("METRICS - one qubit gate - END");
+
+// 			}
+// 			else if (type_op == 2)
+// 			{
+// 				IOUT("METRICS - TWO qubit gate");
+// 				size_t qubit_c = gate->operands[0];
+// 				size_t qubit_t = gate->operands[1];
+				
+// 				size_t last_time_c = last_op_endtime[qubit_c];
+// 				size_t last_time_t = last_op_endtime[qubit_t];
+// 				size_t idled_time_c = gate->cycle - last_time_c;
+// 				size_t idled_time_t = gate->cycle - last_time_t; //get idlying time to introduce decoherence. This assumes "cycle" starts at zero, otherwise gate->cycle-> (gate->cycle - 1)
+// 				last_op_endtime[qubit_c] = gate->cycle  + gate->duration / CYCLE_TIME; //This assumes "cycle" starts at zero, otherwise gate->cycle-> (gate->cycle - 1)
+// 				last_op_endtime[qubit_t] = gate->cycle  + gate->duration / CYCLE_TIME ; //This assumes "cycle" starts at zero, otherwise gate->cycle-> (gate->cycle - 1)
+				
+// 				IOUT("Gate " + gate->name + "("+ std::to_string(gate->operands[0]) + ", " + std::to_string(gate->operands[1]) +") at cycle " + std::to_string(gate->cycle) + " with duration " + std::to_string(gate->duration));
+// 				IOUT("Idled time q_c:" + std::to_string(idled_time_c));
+// 				IOUT("Idled time q_t:" + std::to_string(idled_time_t));
+// 				IOUT("Decoherence time: " + std::to_string(decoherence_time));
+
+// 				// fids[qubit_c] *= std::exp(-(double) idled_time_c/decoherence_time); // Update fidelity with idling-caused decoherence
+// 				// fids[qubit_t] *= std::exp(-(double)idled_time_t/decoherence_time); // Update fidelity with idling-caused decoherence
+
+// 				for (auto i = 0; i < idled_time_c; i++)
+// 					qubit_partition[qubit_c]->introduce_single_qubit_error(qubit_c, std::exp(-(double)1.0/decoherence_time)); //Introduces wait gates
+// 				for (auto i = 0; i < idled_time_t; i++)
+// 					qubit_partition[qubit_t]->introduce_single_qubit_error(qubit_t, std::exp(-(double)1.0/decoherence_time)); //Introduces wait gates
+
+// 				// IOUT("Error status after idlying: ");
+// 				// PRINTER(fids);
+
+// 				qubit_partition[qubit_c]->two_qubit_gate(qubit_c, gatefid_2, gate->name, 0); //Introduces gate fidelity
+// 				qubit_partition[qubit_t]->two_qubit_gate(qubit_t, gatefid_2, gate->name, 1); //Introduces gate fidelity
+
+
+// 				// fids[qubit_c] *=  fids[qubit_t] * gatefid_2; //Update fidelity after gate
+// 				// fids[qubit_t] = fids[qubit_c];  					//Update fidelity after gate
+
+
+
+// 				//TODO - Convert the code into a for loop with range 2, to get the compiler's for optimization (and possible paralellization?)
+// 			}
+// 				// PRINTER(fids);
+// 				// PRINTER(last_op_endtime);
+				
+// 				IOUT("\n NEXT GATE");
+
 // 		}
+// 		size_t end_cycle = circ.back()->cycle + circ.back()->duration/CYCLE_TIME; 
+// 		for (auto qubit=0; qubit < Nqubits; qubit++ )
+// 		{
+// 			size_t idled_time_final = end_cycle - last_op_endtime[i];
+// 			for(auto wait=0; wait < idled_time_final; wait++)
+// 				qubit_partition[qubit]->introduce_single_qubit_error(qubit, std::exp(-(double)1.0/decoherence_time)); //Introduces wait gates
+				
+// 		}
+
+// 		IOUT(" \n\n THE END \n\n ");
+// 		IOUT("Fidelity after idlying: ");
+// 		PRINTER(fids);
+// 		//Concatenating data into a single value, to serve as metric
+// 		return create_output(fids);
+// 	};
+
+// 	void print_status()
+// 	{
+// 		for(auto q_set in qubit_partition)
+// 		{
+// 			DOUT("\n\n")
+// 			DOUT("QUBIT_SET: ");
+// 			std::string output;
+// 			for(auto qubit : q_set->qubits)
+// 				output += std::to_string(qubit) + ",";
+// 			DOUT(output)
+// 			output="";
+// 			for(auto error : q_set->error_map)
+// 				output += error.first + std::to_string(error.second) + "\n";
+// 			DOUT(output)
+
+// 		} 
+// 	}
+
 
 // };
 
