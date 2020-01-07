62a992a0accfc35904ddeaf3e185febac61d854b
Diogo Valada <diogovalada.7@tecnico.ulisboa.pt>
2020-01-07 15:58:28 +0100

    compile_all_files.py : creates new folders for each output saves files with parameters

--- tools/compile_all_files.py
+++ tools/compile_all_files.py
@@ -7,6 +7,15 @@ import sys
 from io import StringIO
 from joblib import Parallel, delayed, cpu_count
 
+#very confusing function!
+def generate_new_dest_dir(input, mapper):
+	folders = [name for name in os.listdir(input) if os.path.isdir(os.path.join(input,name)) and ("mapper="+mapper in name)]
+	if folders:
+		new_number = max([int(folder.split("mapper="+mapper+"_")[1]) for folder in folders])+1
+	else:
+		new_number = 1
+	return "mapper=" + mapper + "_" + str(new_number)
+
 
 if __name__ == "__main__":
 	# parser = argparse.ArgumentParser(description='Validate OpenQL compiler')
@@ -43,9 +52,9 @@ if __name__ == "__main__":
 	measurement = False
 
 	#Some compiler options
-	log_level = 'LOG_WARNING'
+	log_level = 'LOG_NOTHING'
 	scheduler = 'ALAP'
-	mapper = 'maxfidelity'
+	mapper = 'minextendrc'
 	optimize = 'no'
 	scheduler_uniform = 'no'
 	initialplace = 'no'
@@ -54,13 +63,13 @@ if __name__ == "__main__":
 	mapusemoves = 'no'
 	maptiebreak = 'first'
 
-	output_dir_name = 'test_output/mapper=' + mapper
+	# output_dir_name = 'test_output/mapper=' + mapper
 
 	#add other options here (overring the options above will not work! change the value in the options above instead!)
 	ql.set_option("maxfidelity_1qbgatefid", "0.999")
 	ql.set_option("maxfidelity_2qbgatefid", "0.99")
-	ql.set_option("maxfidelity_idlefid", "0.999334")
-	ql.set_option("maxfidelity_outputmode", "average")
+	ql.set_option("maxfidelity_idlefid", "0.9867")
+	ql.set_option("maxfidelity_outputmode", "worst")
 
 	ql.set_option('decompose_toffoli', "no") # = "no";
 	ql.set_option("prescheduler", "yes") # = "yes";
@@ -69,7 +78,6 @@ if __name__ == "__main__":
 	ql.set_option("clifford_postmapper", "yes") # = "yes";
 	ql.set_option("mapinitone2one", "yes") # = "yes";
 	ql.set_option("mapassumezeroinitstate", "no") # = "no";
-	ql.set_option("initialplace", "no") # = "no";
 	ql.set_option("initialplace2qhorizon", "0") # = "0";
 	ql.set_option("maplookahead", "noroutingfirst") # = "noroutingfirst";
 	ql.set_option("mappathselect", "all") # = "all";
@@ -79,24 +87,60 @@ if __name__ == "__main__":
 	ql.set_option("mapselectswaps", "all") # = "all";
 	ql.set_option("mapreverseswap", "yes") # = "yes";
 
+
+	#==== Create output folder
+	all_results_dir = "test_output"
+	result_dir = generate_new_dest_dir(os.path.join(indir, all_results_dir), mapper)
+	output_dir_name = os.path.join(all_results_dir, result_dir)
+	if not os.path.exists(os.path.join(indir, output_dir_name)):
+		os.makedirs(os.path.join(indir, output_dir_name))
+	print(result_dir)
+	print(os.path.join(indir, output_dir_name))
+
+	print("mapper: " +           ql.get_option("mapper") + '\n')
+
 	#=== Serial Compilation
-	# for file in files:
+	for file in files:
+		imported = importlib.import_module(os.path.join(file.replace(".py", "")))
+		try:
+			imported.circuit('test_mapper17.json', scheduler = scheduler, mapper = mapper, uniform_sched = scheduler_uniform, new_scheduler = scheduler_post179,  moves = mapusemoves, maptiebreak = maptiebreak, measurement = measurement, optimize = optimize, initial_placement = initialplace, output_dir_name = output_dir_name, log_level = log_level)
+		except Exception as e:
+			print(str(e))
+
+	# === Parallel Compilation (does not work! Somehow, the ql.set_options don't take effect within each of the worker processes)
+	# def parallel_import_compile(file, scheduler, mapper, scheduler_uniform, scheduler_post179, mapusemoves, maptiebreak, measurement, optimize, output_dir_name, log_level):
 	# 	imported = importlib.import_module(os.path.join(file.replace(".py", "")))
 	# 	try:
-	# 		imported.circuit('test_mapper17.json', scheduler = scheduler, mapper = mapper, uniform_sched = scheduler_uniform, new_scheduler = scheduler_post179,  moves = mapusemoves, maptiebreak = maptiebreak, measurement = measurement, optimize = optimize, output_dir_name = output_dir_name, log_level = log_level)
+	# 		imported.circuit('test_mapper17.json', scheduler = scheduler, mapper = mapper, uniform_sched = scheduler_uniform, new_scheduler = scheduler_post179,  moves = mapusemoves, maptiebreak = maptiebreak, measurement = measurement, optimize = optimize, initial_placement = initialplace, output_dir_name = output_dir_name, log_level = log_level)
 	# 	except Exception as e:
 	# 		print(str(e))
 
-	#=== Parallel Compilation
-	def parallel_import_compile(file):
-		imported = importlib.import_module(os.path.join(file.replace(".py", "")))
-		try:
-			imported.circuit('test_mapper17.json', scheduler = scheduler, mapper = mapper, uniform_sched = scheduler_uniform, new_scheduler = scheduler_post179,  moves = mapusemoves, maptiebreak = maptiebreak, measurement = measurement, optimize = optimize, output_dir_name = output_dir_name, log_level = log_level)
-		except Exception as e:
-			print(str(e))
-	
-	# Parallel(n_jobs=cpu_count(), verbose=8)(delayed(parallel_import_compile)(file) for file in files)
+	# def parallel_import_compile_v2(files):
+	# 	imported_list = [importlib.import_module(os.path.join(file.replace(".py", ""))) for file in files]
+	# 	def _parallel_import_compile_v2(imported):
+	# 		try:
+	# 			imported.circuit('test_mapper17.json', scheduler = scheduler, mapper = mapper, uniform_sched = scheduler_uniform, new_scheduler = scheduler_post179,  moves = mapusemoves, maptiebreak = maptiebreak, measurement = measurement, optimize = optimize, initial_placement = initialplace, output_dir_name = output_dir_name, log_level = log_level)
+	# 		except Exception as e:
+	# 			print(str(e))
+	# 	Parallel(n_jobs=4, verbose=8)(delayed(_parallel_import_compile_v2)(imported) for imported in imported_list)
 
+
+	
+	# Parallel(n_jobs=4, verbose=8)(delayed(parallel_import_compile)(file, scheduler, mapper, scheduler_uniform, scheduler_post179, mapusemoves, maptiebreak, measurement, optimize, output_dir_name, log_level) for file in files)
+	# parallel_import_compile_v2(files)	
+
+	#==== DO NOT TOUCH HERE (meant to provide some redundancy, so that the parameters variable won't stop working when parallel compilation is fixed):
+	#This is because these parameters are only set within each file from qbench
+	ql.set_option("log_level", log_level)
+	ql.set_option("scheduler", scheduler)
+	ql.set_option("mapper", mapper)
+	ql.set_option("optimize", optimize)
+	ql.set_option("scheduler_uniform", scheduler_uniform)
+	ql.set_option("initialplace", initialplace)
+	ql.set_option("scheduler_post179", scheduler_post179)
+	ql.set_option("scheduler_commute", scheduler_commute)
+	ql.set_option("mapusemoves", mapusemoves)
+	ql.set_option("maptiebreak", maptiebreak)
 
 	#============== Store the options in a file
 	parameters = ["unique_output: " + ql.get_option("unique_output") + '\n'  ,
@@ -127,10 +171,14 @@ if __name__ == "__main__":
 				  "mapselectmaxlevel: " + ql.get_option("mapselectmaxlevel") + '\n' ,
 				  "mapselectmaxwidth: " + ql.get_option("mapselectmaxwidth") + '\n' ,
 
+				  "measurement: " + ('yes' if measurement else 'no') + '\n' ,				  
+
 				  "maxfidelity_1qbgatefid: " + ql.get_option("maxfidelity_1qbgatefid") + '\n'  , 
 				  "maxfidelity_2qbgatefid: " + ql.get_option("maxfidelity_2qbgatefid") + '\n'  , 
 				  "maxfidelity_idlefid: "	 + ql.get_option("maxfidelity_idlefid") + '\n'   ,
 				  "maxfidelity_outputmode: " + ql.get_option("maxfidelity_outputmode") + '\n'
 	]
+	print("Wrote files to:", output_dir_name)
+	print("Writing parameters to:", 'test_files', output_dir_name,'parameters.txt')
 	with open(os.path.join('test_files', output_dir_name,'parameters.txt'), 'w') as fopen:
 		fopen.writelines(parameters)
