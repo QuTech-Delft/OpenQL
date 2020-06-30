2a9e1e69c57af19906b591831eb45de2a5042954
Wouter Vlothuizen <wouter.vlothuizen@tno.nl>
2020-06-30 11:17:17 +0200

    coorected version

--- src/version.h
+++ src/version.h
@@ -1,7 +1,8 @@
 /* Version examples:
 	"0.8.0"			release
 	"0.8.0.dev1"	development, see https://www.python.org/dev/peps/pep-0440
+    "0.8.0+dcl3"    local branch patches, before upstreaming
 
 	 NB: OPENQL_VERSION_STRING is also decoded by setup.py
 */
-#define OPENQL_VERSION_STRING "0.8.1.dcl3"
+#define OPENQL_VERSION_STRING "0.8.1+dcl3"
