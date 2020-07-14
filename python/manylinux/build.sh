10cf4b8569fbecdeefc42cd2195bbd0b0836712b
Jeroen van Straten <jeroen.van.straten@gmail.com>
2020-07-14 17:47:12 +0200

    Finalize manylinux wheel build + test

--- python/manylinux/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf dist
-mkdir -p dist
-docker build --pull -t openql-manylinux ../.. -f Dockerfile
-docker run -v `pwd`/dist:/io/dist openql-manylinux python/manylinux/helper.sh
