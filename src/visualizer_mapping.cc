8a919d37943ae9c7d78899d2cf0c01f4a6125226
Tim <nothing.suspicious.going.on.here@gmail.com>
2021-03-04 20:10:27 +0100

    Remove commented out code

--- src/visualizer_mapping.cc
+++ src/visualizer_mapping.cc
@@ -44,8 +44,6 @@ void visualizeMappingGraph(const quantum_program* program, const VisualizerConfi
         QL_FATAL("Circuit contains no cycles! Cannot visualize mapping graph.");
     }
 
-    // printGates(gates);
-
     // Initialize the first cycle with a virtual index = real index mapping.
     for (Int qubitIndex = 0; qubitIndex < amountOfQubits; qubitIndex++) {
         virtualQubits[0].push_back(qubitIndex);
@@ -58,91 +56,28 @@ void visualizeMappingGraph(const quantum_program* program, const VisualizerConfi
             virtualQubits[cycleIndex].push_back(virtualQubits[cycleIndex - 1][qubitIndex]);
         }
         // Update the virtual qubit operands from the gates in this cycle.
-        for (Int qubitIndex = 0; qubitIndex < amountOfQubits; qubitIndex++) {
-            // Check for changes in virtual operand for this qubit in the gates for this cycle.
-            for (const GateProperties &gate : gates) {
-                // Check if the gate's cycle matches the current cycle.
-                if (gate.cycle == cycleIndex) {
-                    const Vec<Int> virtualOperands = gate.virtual_operands;
-                    const Vec<Int> realOperands = gate.operands;
-                    if (virtualOperands.size() != realOperands.size()) {
-                        continue;
-                        // QL_FATAL("Size of virtual operands vector does match size of real operands vector!");
-                    }
-                    // Swap the virtual and real qubits.
-                    for (Int operandIndex = 0; operandIndex < realOperands.size(); operandIndex++) {
-                        const Int virtualQubit = virtualOperands[operandIndex];
-                        const Int realQubit = realOperands[operandIndex];
-                        virtualQubits[cycleIndex][realQubit] = virtualQubit;
-                        virtualQubits[cycleIndex][virtualQubit] = realQubit;
-                    }
+        for (const GateProperties &gate : gates) {
+            // Check if the gate's cycle matches the current cycle.
+            if (gate.cycle == cycleIndex) {
+                const Vec<Int> virtualOperands = gate.virtual_operands;
+                const Vec<Int> realOperands = gate.operands;
+                if (virtualOperands.size() != realOperands.size()) {
+                    QL_DOUT("Size of virtual operands vector does not match size of real operands vector, skipping gate.");
+                    continue;
+                }
+                // Swap the virtual and real qubits.
+                for (Int operandIndex = 0; operandIndex < realOperands.size(); operandIndex++) {
+                    const Int virtualQubit = virtualOperands[operandIndex];
+                    const Int realQubit = realOperands[operandIndex];
+                    virtualQubits[cycleIndex][realQubit] = virtualQubit;
+                    virtualQubits[cycleIndex][virtualQubit] = realQubit;
                 }
             }
         }
     }
 
-    // Vec<Vec<Int>> virtualQubits(imageOutput.circuitData.getAmountOfCycles());
-    // for (Vec<Int> &qubitsInCycle : virtualQubits) {
-    //     for (Int i = 0; i < amountOfQubits; i++) {
-    //         qubitsInCycle.push_back(-1);
-    //     }
-    // }
-
-    // for (const Vec<Int> &qubitsInCycle : virtualQubits) {
-    //     for (Int qubitIndex = 0; qubitIndex < qubitsInCycle.size(); qubitIndex++) {
-    //         QL_IOUT("\tqubit index: " << qubitIndex << " virtual qubit: " << qubitsInCycle);
-    //     }
-    // }
-
-    // printGates(gates);
-
-    // QL_IOUT("Storing virtual operands...");
-
-    // for (const GateProperties &gate : gates) {
-    //     const Int cycleIndex = gate.cycle;
-    //     const Vec<Int> virtualOperands = gate.virtual_operands;
-    //     const Vec<Int> operands = gate.operands;
-    //     QL_IOUT("cycleIndex: " << cycleIndex);
-    //     QL_IOUT("virtualOperands.size(): " << virtualOperands.size());
-    //     QL_IOUT("operands.size(): " << operands.size());
-    //     for (Int i = 0; i < operands.size(); i++) {
-    //         QL_IOUT("\ti: " << i);
-    //         QL_IOUT("\toperands[i]: " << operands[i]);
-    //         QL_IOUT("\tvirtualOperands[i]: " << virtualOperands[i]);
-    //         virtualQubits[cycleIndex][operands[i]] = virtualOperands[i];
-    //     }
-    // }
-
-    // for (const Vec<Int> &qubitsInCycle : virtualQubits) {
-    //     for (Int qubitIndex = 0; qubitIndex < qubitsInCycle.size(); qubitIndex++) {
-    //         QL_IOUT("\tqubit index: " << qubitIndex << " virtual qubit: " << qubitsInCycle);
-    //     }
-    // }
-
     // Draw the mapping for each cycle.
     for (Int cycleIndex = 0; cycleIndex < imageOutput.circuitData.getAmountOfCycles(); cycleIndex++) {
-        // Vec<Int> virtualQubits(amountOfQubits);
-        // const Cycle cycle = imageOutput.circuitData.getCycle(cycleIndex);
-
-        // for (const Vec<std::reference_wrapper<GateProperties>> partition : cycle.gates) {
-        //     for (const GateProperties gate : partition) {
-        //         // QL_IOUT(gate.virtual_operands.size());
-        //         // for (const Int vop : gate.virtual_operands) {
-        //         //     QL_IOUT(vop);
-        //         // }
-        //         Vec<Int> virtualOperands = gate.virtual_operands;
-        //         Vec<Int> operands = gate.operands;
-        //         for (Int i = 0; i < operands.size(); i++) {
-        //             virtualQubits[operands[i]] = virtualOperands[i];
-        //         }
-        //     }
-        // }
-
-        // QL_IOUT("cycle: " << cycleIndex);
-        // for (Int i = 0; i < virtualQubits.size(); i++) {
-        //     QL_IOUT("\t\treal: " << i << " --> virtual: " << virtualQubits[i]);
-        // }
-
         const Position4 position = imageOutput.structure.getCellPosition(cycleIndex, 0, QUANTUM);
         const Int xStart = position.x0;
         const Int xEnd = position.x1;
