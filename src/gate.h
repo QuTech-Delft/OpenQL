d048b5c6f2990ac7934e5f47f31c0eb66e595a70
Tim <nothing.suspicious.going.on.here@gmail.com>
2021-03-17 14:30:22 +0100

    Fix build errors on macos and linux 6

--- src/gate.h
+++ src/gate.h
@@ -188,6 +188,14 @@ struct swap_parameters {
     utils::Int r1 = -1;
     utils::Int v0 = -1;
     utils::Int v1 = -1;
+
+    // default constructor
+    swap_parameters() {}
+
+    // initializer list
+    swap_parameters(utils::Bool _part_of_swap, utils::Int _r0, utils::Int _r1, utils::Int _v0, utils::Int _v1)
+        : part_of_swap(_part_of_swap), r0(_r0), r1(_r1), v0(_v0), v1(_v1)
+    {}
 };
 
 /**
