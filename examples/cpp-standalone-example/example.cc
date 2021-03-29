f332ed07bee72f529b20b0ebfba4c53459d0f1bb
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-03-29 08:40:39 +0200

    Fix standalone example for now

--- examples/cpp-standalone-example/example.cc
+++ examples/cpp-standalone-example/example.cc
@@ -7,23 +7,23 @@ int main(int argc, char ** argv)
   ql::quantum_platform platf("seven_qubits_chip", "hardware_config_cc_light.json");
 
   // create program
-  ql::quantum_program prog("aProgram", platf, 2);
+  auto prog = ql::utils::make_node<ql::ir::Program>("aProgram", platf, 2);
 
   // create kernel
-  ql::quantum_kernel k("aKernel", platf, 2);
+  auto k = ql::utils::make_node<ql::ir::Kernel>("aKernel", platf, 2);
 
-  k.gate("prepz", 0);
-  k.gate("prepz", 1);
-  k.gate("x", 0);
-  k.gate("y", 1);
-  k.measure(0);
-  k.measure(1);
+  k->gate("prepz", 0);
+  k->gate("prepz", 1);
+  k->gate("x", 0);
+  k->gate("y", 1);
+  k->measure(0);
+  k->measure(1);
 
   // add kernel to program
-  prog.add(k);
+  prog->add(k);
 
   // compile the program
-  prog.compile();
+  prog->compile();
 
   std::cout << "Seems good to me!" << std::endl;
   return 0;
