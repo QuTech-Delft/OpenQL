0d07ec1efeb254b5c9786cd20eb1231d90046d53
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-05-25 21:29:29 +0200

    WIP on range map, add unit test system

---
+++ src/ql/utils/rangemap.cc
@@ -0,0 +1,33 @@
+/** \file
+ * A map (and set) mapping from non-overlapping *ranges* of keys to values.
+ */
+
+#include "ql/utils/rangemap.h"
+
+namespace ql {
+namespace utils {
+
+/**
+ * String conversion for Empty.
+ */
+std::ostream &operator<<(std::ostream &os, const Nothing &e) {
+    return os;
+}
+
+/**
+ * String conversion for RangeMatchType.
+ */
+std::ostream &operator<<(std::ostream &os, RangeMatchType rmt) {
+    switch (rmt) {
+        case RangeMatchType::NONE:      return os << "none";
+        case RangeMatchType::PARTIAL:   return os << "partial";
+        case RangeMatchType::MULTIPLE:  return os << "multiple";
+        case RangeMatchType::SUPER:     return os << "super";
+        case RangeMatchType::SUB:       return os << "sub";
+        case RangeMatchType::EXACT:     return os << "exact";
+        default:                        return os << "<UNKNOWN>";
+    }
+}
+
+} // namespace utils
+} // namespace ql
