03e7386d29194c1b50e6ab7aae93cd781ca350d5
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-07-15 17:22:37 +0200

    Ignore first # comment in file_compare
    to avoid false negatives due to different compiler versions in the
    generator header

--- tests/utils.py
+++ tests/utils.py
@@ -3,13 +3,18 @@ import difflib
 
 def file_compare(fn1, fn2):
     """
-    Compare two files and raise an Assertion error if files are different.
+    Compare two cQASM files and raise an Assertion error if they are different,
+    but ignoring for the # comment block that the files start with.
 
     The assertion error contains a diff of the files.
     """
     with open(fn1) as f, open(fn2) as g:
-        flines = f.readlines()
-        glines = g.readlines()
+        flines = list(f.readlines())
+        while flines and flines[0].startswith('#'):
+            flines.pop(0)
+        glines = list(g.readlines())
+        while glines and glines[0].startswith('#'):
+            glines.pop(0)
         d = difflib.Differ()
         diffs = [x for x in d.compare(flines, glines) if x[0] in ('+', '-')]
         if diffs:
