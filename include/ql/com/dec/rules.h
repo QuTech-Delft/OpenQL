c4c2408cad1ea53ed7e2b0990f87df15b5ad8b6d
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-07-15 16:34:23 +0200

    Clarify nested block behavior of insn decomposer

--- include/ql/com/dec/rules.h
+++ include/ql/com/dec/rules.h
@@ -17,12 +17,14 @@ using RulePredicate = std::function<utils::Bool(const ir::DecompositionRef&)>;
 
 /**
  * Recursively applies all available decomposition rules (that match the
- * predicate, if given) to the given block. If ignore_schedule is set, the
- * schedule of the decomposition rules is ignored, and instead the statements
- * in the rule are all given the same cycle number as the original statement.
- * If ignore_schedule is not set, the schedule is copied from the decomposition
- * rule, possibly resulting in instructions being reordered. Returns the number
- * of decomposition rules that were applied.
+ * predicate, if given) to the given block. Sub-blocks are not considered; in
+ * case structured control-flow blocks exist inside the block and these need to
+ * be handled as well, it is the responsibility of the callee to do so. If
+ * ignore_schedule is set, the schedule of the decomposition rules is ignored,
+ * and instead the statements in the rule are all given the same cycle number as
+ * the original statement. If ignore_schedule is not set, the schedule is copied
+ * from the decomposition rule, possibly resulting in instructions being
+ * reordered.
  */
 utils::UInt apply_decomposition_rules(
     const ir::Ref &ir,
