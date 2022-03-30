18497d6912d2ec517f00c5c0208c2bd45af31d19
Hans van Someren <j.vansomeren-1@tudelft.nl>
2022-03-30 19:56:54 +0200

    tests/test_gate_decomposition.py updated to compare output files
    This test file was found not to check on correct output.
    Since decomposition functioning is wrong somewhere, this was necessary to close in the problem.

--- tests/test_gate_decomposition.py
+++ tests/test_gate_decomposition.py
@@ -1,6 +1,7 @@
 from openql import openql as ql
 import unittest
 import os
+from utils import file_compare
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 output_dir = os.path.join(curdir, 'test_output')
@@ -14,32 +15,36 @@ class Tester(unittest.TestCase):
         ql.set_option('output_dir', output_dir)
         ql.set_option('optimize', 'no')
         ql.set_option('scheduler', 'ALAP')
-        ql.set_option('log_level', 'LOG_WARNING')
+        ql.set_option('log_level', 'LOG_DEBUG')
 
     def test_decomposition(self):
         config_fn = os.path.join(curdir, 'test_cfg_none.json')
         platform  = ql.Platform('platform_none', config_fn)
-        sweep_points = [1,2]
         num_qubits = 17
         p = ql.Program('test_decomposition', platform, num_qubits)
-        p.set_sweep_points(sweep_points)
 
         k = ql.Kernel('aKernel', platform, num_qubits)
 
-        k.x(0) # x will be dcomposed
-        k.gate("x", [0]); # x will be dcomposed
+        k.x(0) # x will be decomposed
+        k.gate("x", [0]); # x will be decomposed
         k.gate("y", [0]); # decomposition not available, will use custom gate
         k.gate("s", [1]); # decomposition as well as custom gate not available, will use default gate
         k.gate("roty90", [0]) # any name can be used for composite gate
 
-        k.gate("cnot", [0, 1] ) # cnot will be decomposed
-        k.gate("cnot", [2, 3] ) # same as above but with a list of qubits
+        k.gate("cnot", [0, 1] ) # decomposition overrules custom(=primitive) gate; decomposition uses specialized cz q0,q1
+        k.gate("cnot", [2, 3] ) # cnot will be decomposed; decomposition falls back to default gate for cz q2,q3
 
         # add the kernel to the program
         p.add_kernel(k)
 
         # compile the program
         p.compile()
+
+        prog_name = "test_decomposition"
+        gold_fn = curdir + '/golden/' + prog_name +'_scheduled.qasm'
+        qasm_fn = os.path.join(output_dir, prog_name+'_scheduled.qasm')
+        self.assertTrue(file_compare(qasm_fn, gold_fn))
+
 
 if __name__ == '__main__':
     unittest.main()
