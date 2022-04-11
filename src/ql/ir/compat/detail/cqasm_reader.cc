0cb35fbd3c074f920799401ae0310326e76d1555
Wouter Vlothuizen <wouter.vlothuizen@tno.nl>
2022-04-11 17:00:06 +0200

    moved register_instruction for "skip" out of loop

--- src/ql/ir/compat/detail/cqasm_reader.cc
+++ src/ql/ir/compat/detail/cqasm_reader.cc
@@ -603,8 +603,8 @@ lqa::Analyzer ReaderImpl::build_analyzer() {
     a.register_function("operator!=", "bb", op_lxor_bb);
     for (const auto &gate : gateset) {
         a.register_instruction(gate->cq_insn);
-        a.register_instruction("skip", "i", false, false);  // FIXME: why inside loop
     }
+    a.register_instruction("skip", "i", false, false);
     return a;
 }
 
