12c7cfd4eb6c45a9226f2c1b5f45ef3607b28c8b
rturrado <68099809+rturrado@users.noreply.github.com>
2023-04-25 19:00:14 +0200

    test/ql/ir/cqasm/read.cc: added several fake structures, the main one being PlatformFake. Now the test is simplified a bit.

--- test/ql/ir/cqasm/read.cc
+++ test/ql/ir/cqasm/read.cc
@@ -1,11 +1,70 @@
-#include "ql/ir/ir.h"
+#include "ql/arch/factory.h"
+#include "ql/arch/none/info.h"
+#include "ql/ir/ir.gen.h"
 #include "ql/ir/compat/platform.h"
 #include "ql/ir/cqasm/read.h"
-#include "ql/ir/old_to_new.h"
+#include "ql/rmgr/manager.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+
+struct CompatPlatformFake : public ql::ir::compat::Platform {};
+
+struct FactoryFake : public ql::rmgr::Factory {};
+
+struct ResourceManagerFake : public ql::rmgr::Manager {
+    explicit ResourceManagerFake(
+        const ql::ir::compat::PlatformRef &platform,
+        const ql::utils::Str &architecture = "",
+        const ql::utils::Set<ql::utils::Str> &dnu = {},
+        const ql::rmgr::Factory &factory = {},
+        const ql::ir::Ref &ir = {}
+    ) : ql::rmgr::Manager{ platform, architecture, dnu, factory, ir } {}
+};
+
+struct PlatformFake : public ql::ir::Platform {
+    PlatformFake() {
+        name = "platform_fake";
+
+        auto creg_count = 1;
+        auto qubit_count = 1;
+
+        // Notice data_types and objects vectors have to be sorted
+        // Thus, adding elements in order: bit, int and qubit; and creg and q, respectively
+        auto bit_type = ql::utils::make<ql::ir::BitType>("bit").template as<ql::ir::DataType>();
+        data_types.get_vec().push_back(bit_type);
+
+        auto int_type = ql::utils::make<ql::ir::IntType>("int", true, 32).template as<ql::ir::DataType>();
+        data_types.get_vec().push_back(int_type);
+        auto physical_object = ql::utils::make<ql::ir::PhysicalObject>("creg", int_type, ql::ir::prim::UIntVec(creg_count));
+        objects.get_vec().push_back(physical_object);
+
+        auto qubit_type = ql::utils::make<ql::ir::QubitType>("qubit").template as<ql::ir::DataType>();
+        data_types.get_vec().push_back(qubit_type);
+        physical_object = ql::utils::make<ql::ir::PhysicalObject>("q", qubit_type, ql::ir::prim::UIntVec(qubit_count));
+        objects.get_vec().push_back(physical_object);
+        qubits = physical_object;
+
+        implicit_bit_type = bit_type;
+        default_bit_type = bit_type;
+        default_int_type = int_type;
+
+        auto top = ql::com::Topology(qubit_count, ql::utils::Json{ R"({
+            "number_of_cores":  1,
+            "connectivity": "full",
+            "form": "irregular",
+            "comm_qubits_per_core": 4
+        })" });
+        topology.populate(top);
+
+        auto arch = ql::arch::Factory().build_from_namespace("none");
+        architecture.populate(arch);
+
+        auto rmgr_fake = ResourceManagerFake{ ql::utils::One<CompatPlatformFake>{}, "", {}, FactoryFake{}, {}};
+        resources.populate(rmgr_fake);
+    }
+};
 
 /*
 TEST(read, version_1_0) {
@@ -41,12 +100,7 @@ TEST(read, version_1_2) {
     const ql::utils::Str fname{};
     ql::ir::Ref ir{};
     ir.emplace();
-    auto platform = ql::ir::compat::Platform::build(ql::utils::Str("none"), ql::utils::Str("none"));
-    // TODO:
-    // convert_old_to_new causes read to be called twice
-    // It doesn't make sense to test a function and call it twice during the test setup
-    // Have a look into using a platform mock
-    ir->platform = ql::ir::convert_old_to_new(platform)->platform;
+    ir->platform = ql::utils::make<PlatformFake>();
     ql::ir::cqasm::ReadOptions options{};
     ql::ir::cqasm::read(ir, data, fname, options);
     EXPECT_THAT(ir->program->name, "program");
@@ -74,8 +128,7 @@ TEST(read, version_3_0) {
     const ql::utils::Str fname{};
     ql::ir::Ref ir{};
     ir.emplace();
-    auto platform = ql::ir::compat::Platform::build(ql::utils::Str("none"), ql::utils::Str("none"));
-    ir->platform = ql::ir::convert_old_to_new(platform)->platform;
+    ir->platform = ql::utils::make<PlatformFake>();
     ql::ir::cqasm::ReadOptions options{};
     ql::ir::cqasm::read(ir, data, fname, options);
     EXPECT_THAT(ir->program->name, "program");
@@ -86,8 +139,7 @@ TEST(read, no_version) {
     const ql::utils::Str fname{};
     ql::ir::Ref ir{};
     ir.emplace();
-    auto platform = ql::ir::compat::Platform::build(ql::utils::Str("none"), ql::utils::Str("none"));
-    ir->platform = ql::ir::convert_old_to_new(platform)->platform;
+    ir->platform = ql::utils::make<PlatformFake>();
     ql::ir::cqasm::ReadOptions options{};
     EXPECT_THROW(ql::ir::cqasm::read(ir, data, fname, options), std::runtime_error);
 }
@@ -96,8 +148,7 @@ TEST(read, version_abc) {
     const ql::utils::Str fname{};
     ql::ir::Ref ir{};
     ir.emplace();
-    auto platform = ql::ir::compat::Platform::build(ql::utils::Str("none"), ql::utils::Str("none"));
-    ir->platform = ql::ir::convert_old_to_new(platform)->platform;
+    ir->platform = ql::utils::make<PlatformFake>();
     ql::ir::cqasm::ReadOptions options{};
     EXPECT_THROW(ql::ir::cqasm::read(ir, data, fname, options), std::runtime_error);
 }
@@ -106,8 +157,7 @@ TEST(read, version_1_1_abc) {
     const ql::utils::Str fname{};
     ql::ir::Ref ir{};
     ir.emplace();
-    auto platform = ql::ir::compat::Platform::build(ql::utils::Str("none"), ql::utils::Str("none"));
-    ir->platform = ql::ir::convert_old_to_new(platform)->platform;
+    ir->platform = ql::utils::make<PlatformFake>();
     ql::ir::cqasm::ReadOptions options{};
     EXPECT_THROW(ql::ir::cqasm::read(ir, data, fname, options), std::runtime_error);
 }
