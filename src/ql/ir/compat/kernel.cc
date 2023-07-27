db3b4239d03ad3b619faea5f71e296d5469369ed
rturrado <rturrado@gmail.com>
2023-07-27 16:30:04 +0200

    CMakeLists.txt: - Added 'SYSTEM' to 'add_subdirectory' for highs, so that warnings in highs code are not treated as errors in our project. test.yml: - Fixed way of executing 'example.exe' in Windows. - Fixed '$env:GITHUB_ENV' references.

--- src/ql/ir/compat/kernel.cc
+++ src/ql/ir/compat/kernel.cc
@@ -551,7 +551,7 @@ Bool Kernel::add_custom_gate_if_available(
         if (!instr.empty()) {
             instr += ",";
         }
-        instr += std::string{"q"} + std::to_string(qubit);
+        instr += std::string{"q"} + to_string(qubit);
     }
     instr = gname + " " + instr;
 
@@ -639,7 +639,7 @@ Bool Kernel::add_spec_decomposed_gate_if_available(
         if (!instr_parameterized.empty()) {
             instr_parameterized += ",";
         }
-        instr_parameterized += std::string{"q"} + std::to_string(qubit);
+        instr_parameterized += std::string{"q"} + to_string(qubit);
     }
     instr_parameterized = gate_name + " " + instr_parameterized;
     QL_DOUT("specialized instruction name: " << instr_parameterized);
@@ -737,7 +737,7 @@ Bool Kernel::add_param_decomposed_gate_if_available(
         if (!instr_parameterized.empty()) {
             instr_parameterized += ",";
         }
-        instr_parameterized += std::string{"%"} + std::to_string(i);
+        instr_parameterized += std::string{"%"} + to_string(i);
     }
     instr_parameterized = gate_name + " " + instr_parameterized;
     QL_DOUT("parameterized instruction name: " << instr_parameterized);
