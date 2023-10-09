22e4219c001b4fc55fdbab3ca981c06c648ee3e0
rturrado <68099809+rturrado@users.noreply.github.com>
2023-10-09 16:51:10 +0200

    Removed reference to Python 3.7. [skip ci]

--- setup.py
+++ setup.py
@@ -198,7 +198,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
 
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
