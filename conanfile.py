d6b663ae60f54ce2aebbc049f522d76c57b2703a
rturrado <68099809+rturrado@users.noreply.github.com>
2023-12-18 11:54:10 +0100

    WIP: debugging bit_op_issue.

--- conanfile.py
+++ conanfile.py
@@ -49,8 +49,8 @@ class OpenQLConan(ConanFile):
         self.requires("range-v3/0.12.0")
         if self.settings.arch != "armv8":
             self.tool_requires("zulu-openjdk/11.0.19")
-        if self.options.build_tests:
-            self.requires("gtest/1.14.0")
+        #if self.options.build_tests:
+        self.requires("gtest/1.14.0")
 
     def requirements(self):
         self.requires("antlr4-cppruntime/4.13.0")
@@ -71,8 +71,8 @@ class OpenQLConan(ConanFile):
         self.cpp.build.libdirs = ["."]
 
     def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
+        #deps = CMakeDeps(self)
+        #deps.generate()
         tc = CMakeToolchain(self)
         tc.variables["OPENQL_BUILD_PYTHON"] = self.options.build_python
         tc.variables["OPENQL_BUILD_TESTS"] = self.options.build_tests
